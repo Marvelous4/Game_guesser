8a5e58ce9c91fdb29ffe6627987ebdc35d587fc3
Marvelous Gbenro <mavgbenro@gmail.com>
2019-11-19 23:38:00 +0100

    add comparison of guess to secret number module

--- src/main.rs
+++ src/main.rs
@@ -11,4 +11,10 @@ fn main() {
     io::stdin().read_line(&mut guess).expect("Failed to read line");
 
     println!("You guessed: {}", guess);
+
+    match guess.cmp(&secret_number){
+        Ordering::Less => println!("Too small"),
+        Ordering::Greater => println!("Too big"),
+        Ordering::Equal => println!("You win!"),
+    }
 }
